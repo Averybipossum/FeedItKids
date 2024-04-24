979dd32ceccebb1afaad6d0d50955a46ad4df3d7
Uper05 <jvfdessani@gmail.com>
2024-04-24 10:39:05 -0300

    Teste de Imagem

--- app/bichinho/index.tsx
+++ app/bichinho/index.tsx
@@ -1,23 +1,17 @@
-import { Image } from 'expo-image';
+
 import * as React from 'react';
-import { StyleSheet,Button, View, Text } from 'react-native';
+import { StyleSheet,Button, View, Text,Image} from 'react-native';
 import {router } from "expo-router";
 
 function Teste() {
     return (
       <View style={styles.container}>
         <Text>Bichinho</Text>
-        <Image
-        style={styles.image}
-        source="https://picsum.photos/seed/696/3000/2000"
-        contentFit="cover"
-        transition={1000}
-        />
         <Button
-                title="ir para Home"
-                onPress={()=>router.replace("/home")}
-                color="#841584"
-            />
+          title="ir para Home"
+          onPress={()=>router.replace("/home")}
+          color="#841584"
+        />
       </View>
     );
   }
