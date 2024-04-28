7a78666a446e6571ef9bd761a62547619b1402cf
Uper05 <jvfdessani@gmail.com>
2024-04-28 01:54:59 -0300

    Escolha do Bichinho em desenvolvimento
    Integração de tela de escolha de bichinho

--- app/bichinho/index.tsx
+++ app/bichinho/index.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { StyleSheet,Button, View, Text,Image, ImageBackground} from 'react-native';
+import { StyleSheet,Button, View, Text,Image, ImageBackground, Modal, Pressable,} from 'react-native';
 import {router} from "expo-router";
 import MagmaSlime from "../../assets/MagmaSlime.png";
 import IceSlime from "../../assets/IceSlime.png"
@@ -9,18 +9,37 @@ import { useState } from 'react';
 
 
 function Bichinho() {
+  const [seletorVisivel,setSeletorVisivel] = useState(false);
   const [showFirstImage, setShowFirstImage] = useState(true);
   const firstImage = MagmaSlime;
   const secondImage = IceSlime;
+  let escolhaBichinho = false;
   const changeImageSource = () => {
     setShowFirstImage(!showFirstImage)
+  }
+  const abrirSeletor = ()=>{
+    setSeletorVisivel(true);
+  }
+  const fecharSeletor = ()=>{
+    setSeletorVisivel(false);
   }
   return (
     <View style={styles.container}>
       <ImageBackground source={BGimage} resizeMode="cover" style={styles.imagem}>
         <View style={styles.container}>
-          <Text>Bichinho</Text>
-          <Image style={styles.image} source={showFirstImage?firstImage:secondImage}/>
+          <Image style={styles.imagemBichinho} source={showFirstImage?firstImage:secondImage}/>
+        </View>
+        <View>
+          <Modal
+              animationType='slide'
+              transparent={true}
+              visible={seletorVisivel}
+              onRequestClose={fecharSeletor}>
+                <View style={styles.containerSeletor}>
+                  <View style={styles.conteudoSeletor}>
+                  </View>
+                </View>
+          </Modal>
         </View>
         <View>
         <Button
@@ -30,7 +49,7 @@ function Bichinho() {
           />
         <Button
             title="Mudar Imagem"
-            onPress={changeImageSource}
+            onPress={abrirSeletor}
             color="#841584"
           />
         </View>
@@ -44,7 +63,21 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
-    image: {
+    containerSeletor:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:'rgba(0,0,0,0.5)',
+    },
+    conteudoSeletor:{
+      padding:20,
+    },
+    imagemSeletor:{
+      flex: 1,
+      objectFit:'contain',
+      width:200,
+    },
+    imagemBichinho: {
       flex: 1,
       width: '75%',
       objectFit: 'contain',
