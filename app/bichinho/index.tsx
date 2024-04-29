79753f337b13058bf3aa84853b1cd350759ffe44
Uper05 <jvfdessani@gmail.com>
2024-04-29 09:12:55 -0300

    Finalização da Escolha de Bichinho
    Agora o Modal sempre é aberto quando a imagem do bichinho não é escolhida, e o usuário pode mudar sua escolha quantas vezes quiser

--- app/bichinho/index.tsx
+++ app/bichinho/index.tsx
@@ -2,41 +2,89 @@
 import * as React from 'react';
 import { StyleSheet,Button, View, Text,Image, ImageBackground, Modal, Pressable,} from 'react-native';
 import {router} from "expo-router";
+import { useState,useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Empty from "../../assets/Empty.png"
 import MagmaSlime from "../../assets/MagmaSlime.png";
 import IceSlime from "../../assets/IceSlime.png"
 import BGimage from "../../assets/BGimage.png"
-import { useState } from 'react';
+
 
 
 function Bichinho() {
   const [seletorVisivel,setSeletorVisivel] = useState(false);
-  const [showFirstImage, setShowFirstImage] = useState(true);
-  const firstImage = MagmaSlime;
-  const secondImage = IceSlime;
+  const [baseImageIndex, setBaseImageIndex] = useState(Empty);
   let escolhaBichinho = false;
-  const changeImageSource = () => {
-    setShowFirstImage(!showFirstImage)
-  }
+
   const abrirSeletor = ()=>{
     setSeletorVisivel(true);
   }
   const fecharSeletor = ()=>{
     setSeletorVisivel(false);
   }
+
+  const escolherBichinho = (index: any)=>{
+    setBaseImageIndex(index);
+    saveBaseImageIndex(index)
+    fecharSeletor()
+  }
+
+  const saveBaseImageIndex = async (index:any) => {
+    try {
+      await AsyncStorage.setItem('baseImageIndex', index.toString());
+    } catch (error) {
+      console.error('Erro ao salvar imagem base:', error);
+    }
+  };
+
+  useEffect(() => {
+    const loadBaseImage = async () => {
+      try {
+        const savedIndex = await AsyncStorage.getItem('baseImageIndex');
+        if (savedIndex !== null) {
+          setBaseImageIndex(parseInt(savedIndex));
+        }
+      } catch (error) {
+        console.error('Erro ao carregar imagem base:', error);
+      }
+    };
+    loadBaseImage();
+  }, []);
+
   return (
     <View style={styles.container}>
       <ImageBackground source={BGimage} resizeMode="cover" style={styles.imagem}>
         <View style={styles.container}>
-          <Image style={styles.imagemBichinho} source={showFirstImage?firstImage:secondImage}/>
+          <Image style={styles.imagemBichinho} source={baseImageIndex}/>
         </View>
         <View>
           <Modal
               animationType='slide'
               transparent={true}
-              visible={seletorVisivel}
+              visible={seletorVisivel || baseImageIndex === Empty}
               onRequestClose={fecharSeletor}>
                 <View style={styles.containerSeletor}>
                   <View style={styles.conteudoSeletor}>
+                    <View style={styles.row}>
+                      <Pressable
+                        onPress={()=>escolherBichinho(MagmaSlime)}
+                        style={({pressed})=>[
+                          pressed?{backgroundColor:'green',borderColor:'lightgreen'}:{backgroundColor:'white',borderColor:'lightgreen'},
+                          styles.pressableSeletor
+                        ]}
+                      >
+                        <Image style={styles.imagemSeletor} source={MagmaSlime}></Image>
+                      </Pressable>
+                      <Pressable
+                        onPress={()=>escolherBichinho(IceSlime)}
+                        style={({pressed})=>[
+                          pressed?{backgroundColor:'green',borderColor:'lightgreen'}:{backgroundColor:'white',borderColor:'lightgreen'},
+                          styles.pressableSeletor
+                        ]}
+                      >
+                        <Image style={styles.imagemSeletor} source={IceSlime}></Image>
+                      </Pressable>
+                    </View>
                   </View>
                 </View>
           </Modal>
@@ -50,6 +98,11 @@ function Bichinho() {
         <Button
             title="Mudar Imagem"
             onPress={abrirSeletor}
+            color="#841584"
+          />
+        <Button
+            title="Reset Imagem"
+            onPress={()=>escolherBichinho(Empty)}
             color="#841584"
           />
         </View>
@@ -72,10 +125,22 @@ const styles = StyleSheet.create({
     conteudoSeletor:{
       padding:20,
     },
+    pressableSeletor:{
+      width:150,
+      height:150,
+      margin:5,
+      alignItems:'center',
+      borderWidth:5,
+      borderRadius:25,
+    },
     imagemSeletor:{
-      flex: 1,
-      objectFit:'contain',
-      width:200,
+      flex:1,
+      objectFit: 'contain',
+      width: '95%',
+    },
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
     },
     imagemBichinho: {
       flex: 1,
